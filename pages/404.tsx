4b453986c7a1a65b699674eb5710462d7735889f
Daniel Bílek <daniel.bilek@regiojet.com>
2023-01-25 21:05:39 +0100

    fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,8 +5,9 @@ import Container from '../components/Layouts/Container';
 const Page404 = () => {
 	return (
 		<Background>
-			<Container>
-				<h1 className="text-headline">Stránka nenalezena!</h1>
+			<Container customStyles="h-[720px]">
+				<h1 className="text-headline">Stránka nenalezena</h1>
+				<p>Omlouváme se, ale tahle stránka neexistuje. Zkuste některou z dostupných stránek</p>
 			</Container>
 		</Background>
 	);
