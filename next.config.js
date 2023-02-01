82bcbca84f7b5f3703b3d01fbbe6e770d5802eab
Daniel Bílek <daniel.bilek@regiojet.com>
2023-02-01 15:49:17 +0100

    fixed analytics

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
 			},
 		],
 	},
+	env: {
+		API_KEY: 'AIzaSyA-8ky6Xh0zRtfyGNNepNHaRs0PuPsJ004',
+		AUTH_DOMAIN: 'michal-backend.firebaseapp.com',
+		PROJECT_ID: 'michal-backend',
+		STORAGE_BUCKET: 'michal-backend.appspot.com',
+		MESSAGING_SENDER_ID: '968599058899',
+		APP_ID: '1:968599058899:web:81ef5a69b290c67cc39726',
+		MEASUREMENT_ID: 'G-TPWNW2RLSL',
+	},
 };
 
 module.exports = nextConfig;
